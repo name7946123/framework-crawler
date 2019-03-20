4179b9fa4ada2ae14b1acdfbdd2c197906957c8e
guagua.lai <guagua.lai@wegames.com.tw>
2019-03-20 16:53:54 +0800

    建立Model以及migration設定。
    在CMD中，執行php bin/console doctrine:migrations:migrate

---
+++ src/Entity/Word.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\WordRepository")
+ */
+class Word
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $value;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $counts;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $from;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getValue(): ?string
+    {
+        return $this->value;
+    }
+
+    public function setValue(string $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function getCounts(): ?int
+    {
+        return $this->counts;
+    }
+
+    public function setCounts(int $counts): self
+    {
+        $this->counts = $counts;
+
+        return $this;
+    }
+
+    public function getFrom(): ?string
+    {
+        return $this->from;
+    }
+
+    public function setFrom(string $from): self
+    {
+        $this->from = $from;
+
+        return $this;
+    }
+}
